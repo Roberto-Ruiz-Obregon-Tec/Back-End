8f9cc4db2f4178929ebe0fcb83b1fae0923d49d1
Gabriel Cordova <gabocordova07@gmail.com>
2023-10-18 13:24:45 -0600

    Create event controller

---
+++ controllers/event.controller.js
@@ -0,0 +1,8 @@
+const factory = require('./handlerFactory.controller');
+const Event = require('../models/events.model');
+
+exports.getAllEvents = factory.getAll(Event);
+exports.getEvent = factory.getOne(Event);
+exports.createEvent = factory.createOne(Event);
+exports.updateEvent = factory.updateOne(Event);
+exports.deleteEvent = factory.deleteOne(Event);
