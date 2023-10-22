ac5dca9551f4a4a3bf754c647f2785f092cb8974
rico020202 <ricoroca2@gmail.com>
2023-10-22 14:40:27 -0600

    feat: add scholarships to db

---
+++ models/scholarships.model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const AppError = require('../utils/appError');
+
+const scholarshipSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: [true, 'Nombre de la beca requerido'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Descripción de la beca requerida'],
+    },
+    organization: {
+        type: String,
+        required: [true, 'Organización que patrocina la beca requerida'],
+    },
+    location: {
+        type: String,
+        required: [true, 'Ubicación de la beca requerida'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email de contacto requerido'],
+        validate: [validator.isEmail, 'Email inválido'],
+    },
+    phone: {
+        type: String,
+        required: [true, 'Teléfono de contacto requerido'],
+    },
+    startDate: {
+        type: Date,
+        required: [true, 'Fecha de inicio requerida'],
+    },
+    endDate: {
+        type: Date,
+        required: [true, 'Fecha de fin requerida'],
+    }
+});
+
+// date validation
+courseSchema.pre('validate', function () {
+    if (this.endDate < this.startDate) {
+        throw new AppError(
+            'La fecha final debe ser menor a la fecha inicial',
+            400
+        );
+    }
+});
+
+const Scholarship = mongoose.model('Scholarship', scholarshipSchema);
+
+module.exports = Scholarship;
