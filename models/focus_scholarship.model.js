e5cea4fd88fc12021870de49f719db66f440035c
rico020202 <ricoroca2@gmail.com>
2023-10-22 15:02:17 -0600

    feat: add focus_scholarship to db

---
+++ models/focus_scholarship.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const AppError = require('../utils/appError');
+
+const focusScholarshipSchema = new mongoose.Schema({
+    scholarship: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Scholarship',
+        required: [true, 'Campo de beca necesario'],
+    },
+    focus: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Focus',
+        required: [true, 'Campo de enfoque necesario'],
+    }
+});
+
+// Indexing focusScholarship properties for optimized search
+focusScholarshipSchema.index({ scholarship: 1 });
+focusScholarshipSchema.index({ focus: 1 });
+
+const FocusScholarship = mongoose.model('FocusScholarship', focusScholarshipSchema);
+
+module.exports = FocusScholarship;
