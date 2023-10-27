a03e0d807ea5d88428ed77b56e0f38a7dccdaa3f
ArmandoRosasB <armando.rosas133@gmail.com>
2023-10-26 19:15:27 -0600

    fix: format

--- db/rols.js
+++ db/rols.js
@@ -1,14 +1,14 @@
 const rols = [
     {
-        "_id": 'R001',
-        "name": 'Usuario'
+        "_id": "R001",
+        "name": "Usuario"
     },
     {
-        "_id": 'R002',
-        "name": 'Administrador'
+        "_id": "R002",
+        "name": "Administrador"
     },
     {
-        "_id": 'R003',
-        "name": 'Ponente'
+        "_id": "R003",
+        "name": "Ponente"
     }
 ]
