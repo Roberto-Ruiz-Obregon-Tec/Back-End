d4f2b6a18ebda6b07546c0574a8f9dde113196e6
ricardo020202 <ricoroca2@hotmail.com>
2023-10-30 16:02:41 -0600

    fix: fixed query

--- controllers/companyCertifications.controller.js
+++ controllers/companyCertifications.controller.js
@@ -6,31 +6,31 @@ const APIFeatures = require(`../utils/apiFeatures`);
 const factory = require('./handlerFactory.controller');
 
 // read company
-exports.getAllCompanies = factory.getAll(companyCertification);
+exports.getAllCompanies = factory.getAll(company);
 
-// exports.getAllCompanies = catchAsync(async (req, res, next) => {
-//     const companiesFeatures = new APIFeatures(company.find({}), req.query);
-//     const companies = await companiesFeatures.query;
+exports.getAllCompanies = catchAsync(async (req, res, next) => {
+    const companiesFeatures = new APIFeatures(company.find({}), req.query);
+    const companies = await companiesFeatures.query;
 
-//     for(let i = companies.length - 1; i >= 0; i--){
-//         const companyCertifications = [];
+    for(let i = companies.length - 1; i >= 0; i--){
+        const companyCertifications = [];
 
-//         const certifications = await companyCertification.find({company: companies[i]._id}, {certification: 1, _id: 0}).populate('certification');
+        const certifications = await companyCertification.find({company: companies[i]._id}, {certification: 1, _id: 0}).populate('certification');
         
-//         const mapCertifications = certifications.map((c) => {companyCertifications.push(c.certification.name)})
+        const mapCertifications = certifications.map((c) => {companyCertifications.push(c.certification.name)})
 
-//         if (certifications === null) {
-//             companies[i] = {...companies[i]._doc, "certifications": "Sin certificaciones"};
-//         }
+        if (certifications === null) {
+            companies[i] = {...companies[i]._doc, "certifications": "Sin certificaciones"};
+        }
 
-//         companies[i] = {...companies[i], "certifications": companyCertifications};
-//     }
+        companies[i] = {...companies[i], "certifications": companyCertifications};
+    }
 
-//     res.status(200).json({
-//         status: 'success',
-//         results: companies.length,
-//         data: {
-//           companies,
-//         },
-//     });
-// });
+    res.status(200).json({
+        status: 'success',
+        results: companies.length,
+        data: {
+          companies,
+        },
+    });
+});
