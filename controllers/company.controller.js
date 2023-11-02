0b88307aa1cbf67f49c678f00de8f0571ead726a
unknown <urigopar889@gmail.com>
2023-11-02 16:59:19 -0600

    deleted files

--- controllers/company.controller.js
+++
@@ -1,5 +0,0 @@
-const certifications = require('../models/company.model');
-const factory = require('./handlerFactory.controller');
-
-// read certifications
-exports.getAllCompanies = factory.getAll(company);
