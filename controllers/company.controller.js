f54c97d2845c5999c3f177baa5df4d2f916a4190
RamonaNF <a01423596@tec.mx>
2023-11-04 20:32:51 -0600

    feat: certifications filter

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -9,16 +9,23 @@ const factory = require('./handlerFactory.controller');
 exports.getAllCompanies = factory.getAll(company);
 
 exports.getAllCompanies = catchAsync(async (req, res, next) => {
+    const req_certifications = req.body.certifications || []; // Filtros por certificación
+console.log("FILTRO ", req_certifications)
+
     const companiesFeatures = new APIFeatures(company.find({}), req.query);
     const companies = await companiesFeatures.query;
 
     const certifications = await companyCertification.find().populate('certification'); // Obtenemos las certificaciones registradas
-    
+    console.time("a")
     for(let i = companies.length - 1; i >= 0; i--){
         const companyCertifications = [];
+        let filter = (req_certifications.length === 0)? true:false; // Para verificar si cumple con los filtros
 
         const mapCertifications = certifications.map((c) => { // Agregamos al documento de empresa las certificaciones que tiene
-            if(c.company.toString() == companies[i]._id.toString()) companyCertifications.push(c.certification.name)
+            if(c.company.toString() == companies[i]._id.toString()) {
+                companyCertifications.push(c.certification.name)
+                filter = (req_certifications.includes(c.certification.name))? true : filter;
+            }
         })
 
         if (certifications === null) { // Si la empresa no cuenta con certificaciones
@@ -26,7 +33,10 @@ exports.getAllCompanies = catchAsync(async (req, res, next) => {
         } else { // Sino, agregamos las certificaciones correspondientes
             companies[i] = {...companies[i]._doc, "certifications": companyCertifications};
         }
+
+        if (!filter) companies.splice(i, 1); // Si no coincide con el filtro, la quitamos de la lista
     }
+    console.timeEnd("a")
 
     res.status(200).json({
         status: 'success',
