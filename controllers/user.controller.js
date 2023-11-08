15b8d14af917ed41f5b0dd3851e3b376d236c88d
Manuel Villalpando Linares <A01352033@tec.mx>
2023-11-08 13:04:59 -0600

    docs: add inline coments in code

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,64 +5,71 @@ const factory = require('./handlerFactory.controller');
 const User = require('../models/users.model');
 const UserRol = require('../models/userRol.model');
 const UserFocus = require('../models/userFocus.model');
-const Focus = require('../models/focus.model'); // Para referencia al modelo focus
-const Rol = require('../models/rols.model'); // Para referencia la modelo rol
+const Focus = require('../models/focus.model'); // Reference to the Focus model
+const Rol = require('../models/rols.model'); // Reference to the Rol model
 
-
+// Export controller functions for handling User data
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User, ['topics']);
 exports.createUser = factory.createOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
 
+// Controller function to get all users with additional filtering and population
 exports.getAllUsers = catchAsync(async (req, res, next) => {
-
-    const userFeatures = new APIFeatures(User.find({}, {password: 0}), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+    // Create an instance of APIFeatures for filtering, sorting, limiting, and pagination
+    const userFeatures = new APIFeatures(User.find({}, { password: 0 }), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+    
+    // Execute the query to fetch users
     const users = await userFeatures.query;
 
-    // Manejo de filtros 
-    const req_rol = req.body.rol || ""; // Por rol
-    const req_focus = req.body.focus || []; // Por enfoque
+    // Filter users based on role and focus criteria
+    const req_rol = req.body.rol || ""; // Filter by role
+    const req_focus = req.body.focus || []; // Filter by focus
 
-    const userRols = await UserRol.find().populate('rol'); // Obtener las tablas de usuarios asociados a roles
-    const userFocus = await UserFocus.find().populate('focus'); // Obtener la lista de intereses (focus) asociados al programa
+    // Fetch UserRol and UserFocus data for reference
+    const userRols = await UserRol.find().populate('rol'); // Get the tables of users associated with roles
+    const userFocus = await UserFocus.find().populate('focus'); // Get the list of interests (focus) associated with the program
 
-    for(let i = users.length - 1; i >= 0; i--){
-      const focusList = []
-      let focusFilter = (req_focus.length == 0)?true:false;
+    // Iterate through the users to apply role and focus filtering
+    for (let i = users.length - 1; i >= 0; i--) {
+        const focusList = [];
+        let focusFilter = (req_focus.length == 0) ? true : false;
 
-      const rol = userRols.find (userInfo => userInfo.user.toString() == users[i]._id.toString())
+        // Find the role of the user
+        const rol = userRols.find(userInfo => userInfo.user.toString() == users[i]._id.toString());
 
-      if (rol === undefined || rol === null) { // Si el usuario no tiene rol
-        users[i] = {...users[i]._doc, "rol": "Sin rol asignado"};
-      } else { // Agregando el campo de rol
-        users[i] = {...users[i]._doc, "rol": rol.rol.name};
-      }
+        if (rol === undefined || rol === null) { // If the user has no role
+            users[i] = { ...users[i]._doc, "rol": "No assigned role" };
+        } else { // Add the role field
+            users[i] = { ...users[i]._doc, "rol": rol.rol.name };
+        }
 
-      const mapFocus = userFocus.map((f) => {
-        if (f.user.toString() === users[i]._id.toString()) { // Buscamos si los intereses del filtro coinciden con los del programa
-            focusList.push(f.focus.name)
-            focusFilter = req_focus.includes(f.focus.name) ? true : focusFilter;
-        } 
-    
-      }) // Almacenando los nombres de los intereses en la lista programaFocus
+        // Check and filter based on user's focus
+        const mapFocus = userFocus.map(f => {
+            if (f.user.toString() === users[i]._id.toString()) {
+                focusList.push(f.focus.name);
+                focusFilter = req_focus.includes(f.focus.name) ? true : focusFilter;
+            }
+        });
 
-      users[i] = { ...users[i], "focus": focusList}; // Agregamos la lista de intereses     
+        users[i] = { ...users[i], "focus": focusList }; // Add the list of interests
 
-      if ((req_rol !== "" && users[i].rol != req_rol) || !focusFilter) {  // Si no coincide el filtro de rol con el del usuario
-        users.splice(i, 1); // Eliminamos el registro
-      }
+        if ((req_rol !== "" && users[i].rol != req_rol) || !focusFilter) {
+            users.splice(i, 1); // Remove the record if it doesn't match the role or focus filter
+        }
     }
 
+    // Send the filtered user data as a response
     res.status(200).json({
-      status: 'success',
-      results: users.length,
-      data: {
-        users,
-      },
+        status: 'success',
+        results: users.length,
+        data: {
+            users,
+        },
     });
-  });
+});
--- routes/topics.route.js
+++ routes/topics.route.js
@@ -9,19 +9,37 @@ const {
     updateTopic,
     deleteTopic,
 } = require(`${__dirname}/../controllers/topics.controller.js`);
+
 const {
     protect,
     restrictTo,
 } = require(`${__dirname}/../controllers/authentication.controller.js`);
 
+// Define routes for handling topics
+
+// Route for getting all topics (GET /topics)
 router
     .route('/')
-    .get(getAllTopics)
+    .get(getAllTopics);
+
+// Route for creating a new topic (POST /topics) - Access restricted to 'Admin' users
+router
+    .route('/')
     .post(protect, restrictTo('Admin'), createTopic);
+
+// Route for getting a specific topic by ID (GET /topics/:id)
 router
     .route('/:id')
-    .get(getTopic)
-    .patch(protect, restrictTo('Admin'), updateTopic)
+    .get(getTopic);
+
+// Route for updating a specific topic by ID (PATCH /topics/:id) - Access restricted to 'Admin' users
+router
+    .route('/:id')
+    .patch(protect, restrictTo('Admin'), updateTopic);
+
+// Route for deleting a specific topic by ID (DELETE /topics/:id) - Access restricted to 'Admin' users
+router
+    .route('/:id')
     .delete(protect, restrictTo('Admin'), deleteTopic);
 
 module.exports = router;
