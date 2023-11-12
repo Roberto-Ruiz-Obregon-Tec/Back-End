d3a2f2bb19fd157b144f15305d9244fa852e1ec2
RamonaNF <a01423596@tec.mx>
2023-11-11 22:47:20 -0600

    feat: createPublication documentation

--- controllers/publication.controller.js
+++ controllers/publication.controller.js
@@ -7,9 +7,9 @@ exports.createPublication = catchAsync(async (req, res, next) => {
     const error = new AppError('No hay datos para crear la publicación', 404);
     const {...publicationInfo} = req.body;
 
-    if(publicationInfo === undefined) return next(error);
+    if(publicationInfo === undefined) return next(error); // Si no se recibió información, se manda un error
 
-    await Publication.create(publicationInfo);
+    await Publication.create(publicationInfo); // Se registra en la base de datos
 
     res.status(200).json({
         status: 'success'
