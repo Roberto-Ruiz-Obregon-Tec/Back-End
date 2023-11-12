13acbdb76c913fa01ea1ac2af74e25f3f748a9f5
ArmandoRosasB <armando.rosas133@gmail.com>
2023-11-11 21:46:43 -0600

    feat:borrar publicacion y comentarios asociados

--- controllers/publication.controller.js
+++ controllers/publication.controller.js
@@ -6,9 +6,9 @@ const CommentPublication = require('../models/commentPublication.model')
 const Comment = require('../models/comments.model')
 
 exports.deletePublication = catchAsync( async (req, res, next) => {
-    const missingError = new AppError('No se recibio nignuna id', 404); // Defino un error en caso de que no se mande el id del programa a eliminar
+    const missingError = new AppError('No se recibio nignuna id', 404); // Defino un error en caso de que no se mande el id de la publicacion a eliminar
 
-    const validationError = new AppError('id no valida', 404); // Defino un error en caso de que no se mande el id del programa a eliminar
+    const validationError = new AppError('id no valida', 404); // Defino un error en caso de que no se mande el id de la publicacion a eliminar
 
 
     if (req.params.id === undefined || req.params.id === null) return next(missingError); // Si no existe id en el body mandamos error
@@ -17,13 +17,20 @@ exports.deletePublication = catchAsync( async (req, res, next) => {
 
     if (!(mongoose.isValidObjectId(id))) return next(validationError); // Si el id no es valido, mandamos error
 
-    // Borramos los enfoques asociados al programa y el programa
-   // await Publication.deleteOne({_id: id});
+    // Borramos la publicacion
+    await Publication.deleteOne({_id: id});
     
-    const comments_id = await CommentPublication.find({publication: id}, {comment: 1});
-    console.log(comments_id)
+    // Borramos los comentarios asociados a la publicacion
+    const comments_id = await CommentPublication.find({publication: id}, {comment: 1, _id : 0});
 
-   // await CommentPublication.deleteMany({publication : id});
+
+    // Borramos los comentarios asociados a la publicacion
+    await CommentPublication.deleteMany({publication : id});
+    
+    // Borramos los comentarios
+   comments_id.forEach(async (c) => {
+        await Comment.deleteOne({_id : c.comment})
+   });
 
 
     res.status(200).json({
