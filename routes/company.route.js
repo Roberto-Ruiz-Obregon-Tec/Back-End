a1f995aeb2d2fffa09508252937e8244858de2d9
rico020202 <ricoroca2@gmail.com>
2023-11-12 14:35:31 -0600

    fix: uncomment route

--- routes/company.route.js
+++ routes/company.route.js
@@ -14,16 +14,16 @@ const {
 } = require(`${__dirname}/../controllers/authentication.controller.js`);
 
 
-// router.route('/').get(
-//     protect, // Validar inicio de sesión
-//     restrictTo('Consultar empresas'), // Validar servicio asociado al rol
-//     getAllCompanies 
-// );
+router.route('/').get(
+    protect, // Validar inicio de sesión
+    restrictTo('Consultar empresas'), // Validar servicio asociado al rol
+    getAllCompanies 
+);
 
-// router.use(protect, restrictTo('Dar de baja empresas del catálogo ESR'));
-// router.route('/delete/:id').delete(deleteCompany);
+router.use(protect, restrictTo('Dar de baja empresas del catálogo ESR'));
+router.route('/delete/:id').delete(deleteCompany);
 
-// router.use(protect, restrictTo('Editar catálogo de empresas ESR'));
+router.use(protect, restrictTo('Editar catálogo de empresas ESR'));
 router.route('/update').put(updateCompany);
 
 module.exports = router;
