17797a1cc9af45669da0ae6dd9e50812bb17a5e3
RamonaNF <a01423596@tec.mx>
2023-11-12 21:00:49 -0600

    feat: add route protection

--- routes/scholarships.route.js
+++ routes/scholarships.route.js
@@ -19,6 +19,6 @@ router.route('/').get(getScholarships);
 router.use(protect, restrictTo('Crear becas'));
 router.route('/create').post(createScholarship);
 
-router.route('/update').put(updateScholarship);
+router.route('/update').put(protect, restrictTo('Editar becas'), updateScholarship);
 
 module.exports = router;
