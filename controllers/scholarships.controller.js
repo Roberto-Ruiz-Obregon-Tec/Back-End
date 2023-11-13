05a67648655b10a27548bb8423147a926c855045
Bromaeg <A01709653@tec.mx>
2023-11-13 11:06:42 -0600

    HotFix: Cambios en el controlador de becas

--- controllers/scholarships.controller.js
+++ controllers/scholarships.controller.js
@@ -2,6 +2,7 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const APIFeatures = require(`../utils/apiFeatures`);
+const mongoose = require('mongoose');
 const scholarships = require('../models/scholarships.model');
 const factory = require('./handlerFactory.controller');
 
