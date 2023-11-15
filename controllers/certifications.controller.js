fae687dde7abbbb25df2aa7cbe0c74ce29c25dd8
Gabriel Cordova <gabocordova07@gmail.com>
2023-11-14 18:14:31 -0600

    Add filters to certifications

--- controllers/certifications.controller.js
+++ controllers/certifications.controller.js
@@ -1,5 +1,34 @@
 const certifications = require('../models/certifications.model');
-const factory = require('./handlerFactory.controller');
+const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('../utils/apiFeatures');
 
-// read certifications
-exports.getAllCertifications = factory.getAll(certifications);
+exports.getAllCertifications = catchAsync(async (req, res) => {
+    const partialName = req.query.name; // Get the partial name from the query parameter
+
+    // Create a regular expression pattern to match the partial name case-insensitively
+    const regexPattern = new RegExp(partialName, 'i');
+
+    // Use the regex pattern in the query to find matching certifications
+    const documents = await certifications.find({ name: regexPattern });
+
+    res.status(200).json({
+        status: 'success',
+        results: documents.length,
+        data: documents,
+    });
+});
+
+exports.getCertification = catchAsync(async (req, res) => {
+    const features = new APIFeatures(certifications.findOne({_id: req.params.id}), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+    
+    const documents = await features.query;
+    
+    res.status(200).json({
+        status: 'success',
+        data: documents,
+    });
+})
