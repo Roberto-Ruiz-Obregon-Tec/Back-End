53731039f6c716eed95f840a8a280a3671a4931f
ArmandoRosasB <armando.rosas133@gmail.com>
2023-11-20 22:42:38 -0600

    feat: consultar inscripciones

--- routes/inscription.route.js
+++ routes/inscription.route.js
@@ -18,8 +18,8 @@ router.use(protect);
 router.route('/inscribeTo').post(restrictTo('User'), inscribeTo);
 router.route('/myInscriptions').get(restrictTo('User'), myInscriptions);
 
-router.use(restrictTo('Admin'));
-router.route('/').get(getAllInscriptions).post(createInscription);
+
+router.route('/').get(protect, restrictTo('Consultar comprobantes de pago'),getAllInscriptions)
 router.route('/:id').get(getInscription).delete(deleteInscription);
 
 module.exports = router;
