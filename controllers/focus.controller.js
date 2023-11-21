c3ef358071489cf3d89c9edb23c930b28684f1f2
Ian326 <a01708940@tec.mx>
2023-11-21 16:39:49 -0600

    fix: Restored updateFocus func

--- controllers/focus.controller.js
+++ controllers/focus.controller.js
@@ -23,6 +23,25 @@ exports.getFocus = catchAsync(async (req, res, next) => {
     });
 });
 
+exports.updateFocus = catchAsync(async (req, res, next) => {
+    const error = new AppError('No existe un enfoque con ese ID', 404);
+    const {_id, name} = req.body;
+
+    if(!mongoose.isValidObjectId(_id)) return next(error);
+
+    const prevFocus = await Focus.findOne({"_id": _id}); // Si no se encuentra el enfoque
+    if(!prevFocus) return next(error); // Se retorna un mensaje de error
+
+    if(name) { // En caso de haber recibido un nombre
+        prevFocus.name = name; // Se actualiza
+        prevFocus.save(); // Se guardan los cambios
+    }
+
+    res.status(200).json({
+        status: 'success'
+    });
+});
+
 exports.deleteFocus = catchAsync(async (req, res, next) => {
     const error = new AppError('No existe un enfoque con ese ID', 404);
 
